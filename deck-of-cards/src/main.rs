03e1c31c438887e8e82545b3eab0787b2b5982c2
saikrishna2313 <ndsdkvarma6823@gmail.com>
2025-12-16 16:16:25 +0530

    Structs Basics

--- deck-of-cards/src/main.rs
+++ deck-of-cards/src/main.rs
@@ -1,24 +1,27 @@
 
 #[derive(Debug)]
-struct Deck{
-
+struct Deck{  
     cards:Vec<String>
-
 }
 
 impl Deck {
 
     fn new()->Self{
-         
          Deck { cards:vec![] }
-
     }
     
+    fn add_cards(&mut self,cards:Vec<String>){
+       self.cards=cards;
+    }
 }
 fn main() {
+    let mut deck=Deck::new();
+
+    println!("{:#?}",deck); 
     
-     let mut deck=Deck::new();
-     
-     println!("{:#?}",deck);
- 
+    let cards=vec![String::from("Ace"),String::from("Queen"),String::from("King")];
+
+    deck.add_cards(cards);
+
+    println!("{:#?}",deck);
 }
